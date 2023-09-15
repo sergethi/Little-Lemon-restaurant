587d9fdaf30ec5f80803e2524d42ddf3b3afcb5c
sergethi <nsergearistide@gmail.com>
2023-09-15 17:23:17 -0400

    added nav footer elements and meta tags

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,74 @@
-import React from 'react'
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
     <footer>
-      Footer
+      <div className="footer-image">
+        <image />
+      </div>
+      <div className="footer-naviagtion">
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">menu</a>
+          </li>
+          <li>
+            <a href="#">reservations</a>
+          </li>
+          <li>
+            <a href="#">Order online</a>
+          </li>
+          <li>
+            <a href="#">Login</a>
+          </li>
+        </ul>
+      </div>
+      <div className="footer-contact">
+        <ul>
+          <li>
+            <a href="#">Contacts</a>
+          </li>
+          <li>
+            <a href="#"> Address</a>
+          </li>
+          <li>
+            <a href="#">Phone number</a>
+          </li>
+          <li>
+            <a href="#">email</a>
+          </li>
+        </ul>
+      </div>
+      <div className="footer-socials">
+        <ul>
+          <li>
+            <a href="#">Socials Links</a>
+          </li>
+          <li>
+            <a href="#">Meta</a>
+          </li>
+          <li>
+            <a href="#">Instagran</a>
+          </li>
+          <li>
+            <a href="#">Twiter</a>
+          </li>
+          <li>
+            <a href="#">Youtube</a>
+          </li>
+          <li>
+            <a href="#"></a>
+          </li>
+        </ul>
+      </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import logo from '../images/Logo-v1.png';
+import './NavigationBar.css'
 
 function NavigationBar() {
   return (
     <nav>
-        <div>
+        <div className='logo-container'>
             <img src={logo} alt=''/>
         </div>
         <ul>
             <li><a href='/home'>Home</a></li>
             <li><a href='/about'>About</a></li>
             <li><a href='/menu'>Menu</a></li>
-            <li><a href='/reservation'>Reservation</a></li>
+            <li><a href='/reservation'>Reservations</a></li>
             <li><a href='/order-onlin'>Order online</a></li>
             <li><a href='/login'>Login</a></li>
         </ul>
